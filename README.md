d978347302b478970f51028b92ba1857e0a3babc
yusanshi <meet.leiyu@gmail.com>
2023-09-26 17:54:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# USTC CAS 密信
+
+利用 USTC CAS 给同学展示密信（使用场景示例：你是助教，给学生自助查分）。
+
+## 快速开始
+
+首先，在个人 USTC FTP 空间放入 `cas-redirect.html` 并对应修改 `backend.py` 中的 `REDIRECT_URL`（若其中的 `REDIRECT_URL` 正常可访问，你也可以省略这一步，即直接用我的 FTP 空间）；
+
+接着，在你的服务器上执行以下操作：
+
+1. `git clone https://github.com/yusanshi/ustc-cas-secret && cd ustc-cas-secret`
+1. 创建 `data.json`，仿照 `data.sample.json` 在其中写入你想要展示给每个人的数据（学号中的字母大写，数据支持 HTML）；
+3. `pip install fastapi "uvicorn[standard]"`；
+4. `python backend.py`；
+4. 设置反向代理绑定到 `http://127.0.0.1:8088`（如 Nginx 服务器使用 proxy_pass）。
+
+
+## 致谢
+
+- [zzh1996/ustccas-revproxy](https://github.com/zzh1996/ustccas-revproxy)
+- [USTC-CAS-Redirect](https://github.com/volltin/USTC-CAS-Redirect)
+
---
+++ backend.py
@@ -0,0 +1,60 @@
+"""based on https://github.com/zzh1996/ustccas-revproxy/blob/master/auth/auth_server.py"""
+
+import json
+import uvicorn
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.responses import RedirectResponse
+from urllib.parse import urlencode
+from xml.etree import ElementTree
+from urllib.request import urlopen
+
+PORT = 8088
+VALIDATE_URL = "https://passport.ustc.edu.cn/serviceValidate"
+CAS_URL = "https://passport.ustc.edu.cn/login"
+REDIRECT_URL = "http://home.ustc.edu.cn/~liulangcao/cas-redirect.html"
+
+app = FastAPI()
+
+with open('data.json') as f:
+    data = json.load(f)
+
+
+def check_ticket(ticket, service):
+    validate = VALIDATE_URL + "?" + urlencode({
+        "service": service,
+        "ticket": ticket
+    })
+    with urlopen(validate) as req:
+        tree = ElementTree.fromstring(req.read())[0]
+    cas = "{http://www.yale.edu/tp/cas}"
+    if tree.tag != cas + "authenticationSuccess":
+        return None
+    uid = tree.find(cas + "user").text.strip().upper()
+    return uid
+
+
+@app.get('/')
+def main(request: Request, ticket=None):
+    service = REDIRECT_URL + "?" + urlencode({"target": request.base_url})
+    if ticket is None or (uid := check_ticket(ticket, service)) is None:
+        return RedirectResponse(CAS_URL + "?" +
+                                urlencode({"service": service}))
+
+    return HTMLResponse(f"""
+    <html>
+        <head>
+            <title>USTC CAS 密信</title>
+        </head>
+        <body style="margin:30px">
+            <h3><a style="text-decoration:none;color:initial;" target="_blank" href="https://github.com/yusanshi/ustc-cas-secret">USTC CAS 密信</a></h3>
+            <p>Hi {uid}！{'以下是你的密信：' if uid in data else '找不到你的密信！'}</p>
+            <div>{data.get(uid,'')}<div>
+        </body>
+    </html>
+    """)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=PORT)
